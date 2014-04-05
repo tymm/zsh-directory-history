39bf10c8c89338d39dd2c697c15abc227311d8f7
Timm <tymmm1@gmail.com>
2014-04-05 13:19:19 +0200

    Changed to 2-space intent

--- directory-history.plugin.zsh
+++ directory-history.plugin.zsh
@@ -3,23 +3,23 @@ unsetopt autonamedirs
 
 # Generates a new history for the current directory
 function generate_history() {
-	history_dir=("${(@f)$(dirhist -a -d $PWD)}")
-	export history_dir
-	MAX_INDEX_HISTORY=$#history_dir
-	export MAX_INDEX_HISTORY
-	(( INDEX_HISTORY = $#history_dir + 1 ))
-	export INDEX_HISTORY
+  history_dir=("${(@f)$(dirhist -a -d $PWD)}")
+  export history_dir
+  MAX_INDEX_HISTORY=$#history_dir
+  export MAX_INDEX_HISTORY
+  (( INDEX_HISTORY = $#history_dir + 1 ))
+  export INDEX_HISTORY
 }
 
 # Append to history file
 function log_command() {
-	dirlog $1 $PWD
+  dirlog $1 $PWD
 }
 
 # Export the current directory
 function log_directory() {
-	DIR_HISTORY=$PWD
-	export DIR_HISTORY
+  DIR_HISTORY=$PWD
+  export DIR_HISTORY
 }
 
 # Call log_directory() everytime the directory is changed
@@ -41,33 +41,33 @@ preexec_functions=(${preexec_functions[@]} "generate_history")
 history_dir=("${(@f)$(dirhist -a -d $PWD)}")
 
 directory-history-search-forward() {
-	# Go forward as long as possible; Last command is at $history_dir[1]
-	if [[ $INDEX_HISTORY -ne 1 ]] && (( INDEX_HISTORY-- ))
+  # Go forward as long as possible; Last command is at $history_dir[1]
+  if [[ $INDEX_HISTORY -ne 1 ]] && (( INDEX_HISTORY-- ))
 
-	# Get command and put it into the buffer
-	COMMAND=$history_dir[$INDEX_HISTORY]
-	zle kill-whole-line
-	BUFFER=$COMMAND
-	zle end-of-line
-	zle vi-backward-char
+  # Get command and put it into the buffer
+  COMMAND=$history_dir[$INDEX_HISTORY]
+  zle kill-whole-line
+  BUFFER=$COMMAND
+  zle end-of-line
+  zle vi-backward-char
 }
 
 directory-history-search-backward() {
-	# Go back as long as possible; First command is at $history_dir[$MAX_INDEX_HISTORY]
-	if [[ $INDEX_HISTORY -ne (( $MAX_INDEX_HISTORY + 1 )) ]] && (( INDEX_HISTORY++ ))
+  # Go back as long as possible; First command is at $history_dir[$MAX_INDEX_HISTORY]
+  if [[ $INDEX_HISTORY -ne (( $MAX_INDEX_HISTORY + 1 )) ]] && (( INDEX_HISTORY++ ))
 
-	# If index is greater than the maximal index
-	if [[ $INDEX_HISTORY -gt $MAX_INDEX_HISTORY ]]; then
-		# Go back to blank
-		BUFFER=""
-	else
-		# Get command and put it into the buffer
-		COMMAND=$history_dir[$INDEX_HISTORY]
-		zle kill-whole-line
-		BUFFER=$COMMAND
-		zle end-of-line
-		zle vi-backward-char
-	fi
+  # If index is greater than the maximal index
+  if [[ $INDEX_HISTORY -gt $MAX_INDEX_HISTORY ]]; then
+    # Go back to blank
+    BUFFER=""
+  else
+    # Get command and put it into the buffer
+    COMMAND=$history_dir[$INDEX_HISTORY]
+    zle kill-whole-line
+    BUFFER=$COMMAND
+    zle end-of-line
+    zle vi-backward-char
+  fi
 }
 
 zle -N directory-history-search-backward
