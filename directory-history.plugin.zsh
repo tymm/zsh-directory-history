8e7f51bffa821df4ff7f3571f0e839148fcb2bcf
Alan Pearce <alan@alanpearce.uk>
2016-10-11 14:04:39 +0200

    Don’t append extraneous newlines to history

--- directory-history.plugin.zsh
+++ directory-history.plugin.zsh
@@ -12,7 +12,7 @@ function generate_history() {
 
 # Append to history file
 function log_command() {
-  echo ": ${EPOCHSECONDS}:0;${PWD};${1}\0\n" >> ~/.directory_history
+  echo -n ": ${EPOCHSECONDS}:0;${PWD};${1}\0\n" >> ~/.directory_history
 }
 
 # Refresh substring search
